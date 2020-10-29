aa5c97c53369d985974e6769b84d1b4b8b2bef80
mohamed amin <mohamedamin.tech@gmail.com>
2020-10-29 04:13:59 +0200

    update file running_blog.sql

--- running_blog.sql
+++ running_blog.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 27, 2020 at 02:35 PM
+-- Generation Time: Oct 29, 2020 at 03:11 AM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.4.9
 
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `testing`
+-- Database: `running_blog`
 --
 
 -- --------------------------------------------------------
